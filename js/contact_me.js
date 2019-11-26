16c3b310757314c856d53c248c2ae00fb79835cd
Jeff Cardinal <jeff@Jeffs-MBP.attlocal.net>
2019-11-26 03:13:14 -0500

    Fix

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      window.open('mailto:jeffjcardinal@gmail.com?subject=' + subject + '&body=' body);
+      window.open('mailto:jeffjcardinal@gmail.com?subject=' + subject + '&body=' + body);
       // $.ajax({
       //   url: "././mail/contact_me.php",
       //   type: "POST",
